8292a7c32ea039bcaf8cd568a0c7afdb73014827
Neel Prajapati <prajapatineel122002@gmail.com>
2026-04-02 17:39:02 +0530

    Cleanup for main

--- src/components/common/table/index.ts
+++
@@ -1,7 +0,0 @@
-export { default as BasicTable } from "./BasicTable"
-export { default as TablePagination } from "./TablePagination"
-export { default as TableActions } from "./TableActions"
-export { default as TableHeader } from "./TableHeader"
-
-export * from "./types"
-export * from "./columnConfig"
